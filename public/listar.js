97a9007212459ffb68ed55d539a8663c39417e90
Paola <paoalm1@hotmail.com>
2023-10-29 19:51:49 -0500

    feat: se añaden detalles a la pagina principal y funciones a los archivos javascript

--- public/listar.js
+++ public/listar.js
@@ -1,44 +1,96 @@
-alert("hello world")
-function cargarLista() {
-  fetch('/list')
+function cargarLista(foldername = '') {
+  fetch('/list/'+ foldername)
     .then((response) => response.json())
     .then((data) => {
-      const listaArchivos = document.getElementById('listaArchivos');
-      const ul = document.createElement('ul');
-
+      const listaArchivos = document.getElementById("listaArchivos");
+      listaArchivos.innerHTML = "";
       data.forEach((archivo) => {
-        const li = document.createElement('li');
-        li.textContent = archivo.name;
-        ul.appendChild(li);
-      });
+        const div = document.createElement("div");
+        div.className = "file";
+        div.textContent = archivo.name;
+        if (archivo.type === 'd') {
+          div.style.cursor = 'pointer';
+          div.addEventListener('click', () => {
+            cargarLista(archivo.name);
+          });
+        }
+        const btnEliminar = document.createElement("button");
+        btnEliminar.textContent = "Eliminar";
+        btnEliminar.addEventListener("click", () => {
+          fetch("/delete/" + archivo.name, { method: "DELETE" })
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error("Error al eliminar el archivo");
+              }
+              return response.text();
+            })
+            .then((message) => {
+              console.log(message);
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+          cargarLista();
+        });
+        const btnDescargar = document.createElement("button");
+        btnDescargar.textContent = "Descargar";
+        btnDescargar.addEventListener("click", () => {
+          const link = document.createElement("a");
+          link.href = "/download/" + archivo.name;
+          link.download = archivo.name;
+          link.click();
+        });
 
-      listaArchivos.innerHTML = '';
-      listaArchivos.appendChild(ul);
+        div.appendChild(btnEliminar);
+        div.appendChild(btnDescargar);
+        listaArchivos.appendChild(div);
+      });
     })
     .catch((error) => {
-      console.error('Error al cargar la lista de archivos:', error);
+      console.error("Error al cargar la lista de archivos:", error);
     });
 }
-
 cargarLista();
+document
+  .getElementById("uploadForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Evitar que el formulario se envíe de la forma predeterminada
 
+    const formData = new FormData(this); // Crear un objeto FormData a partir del formulario
 
+    fetch("/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error al subir el archivo");
+        }
+        return response.text();
+      })
+      .then((message) => {
+        console.log(message);
+        cargarLista(); // Recargar la lista de archivos después de subir
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+  function crearCarpeta() {
+  const nombreCarpeta = document.getElementById('nombreCarpeta').value;
 
-// <!DOCTYPE html>
-// <html>
-// <head>
-//   <title>Subir Archivo</title>
-//   <link rel="stylesheet" type="text/css" href="style.css">
-// </head>
-// <body>
-//   <h1>Subir Archivo al Servidor FTP</h1>
-//   <form action="/upload" method="post" enctype="multipart/form-data">
-//     <input type="file" name="file" accept=".txt, .jpg, .png, .pdf">
-//     <button type="submit">Subir</button>
-//   </form>
-
-//   <div id="listaArchivos"></div>
-//   <script type="text/javascript" src="listar.js" ></script>
-// </body>
-
-// </html>
+  fetch('/create/' + nombreCarpeta, { method: 'POST' })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Error al crear la carpeta');
+      }
+      return response.text();
+    })
+    .then((message) => {
+      console.log(message);
+      cargarLista();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
